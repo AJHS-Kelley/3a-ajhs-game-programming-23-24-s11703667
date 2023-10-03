3ee01dbe1433a6542ea1553ad888292d54105e00
s11703667 <142252946+s11703667@users.noreply.github.com>
2023-10-03 12:04:38 -0400

    Update collections.py

--- gaming_exercises/00-numberGuess/00-collections/collections.py
+++ gaming_exercises/00-numberGuess/00-collections/collections.py
@@ -29,3 +29,21 @@ classGPA = [3.14, 2.25, 1.74, 0.99, 4.25]
 # print(testScores[2])
 # print(classGPA[2])
 
+# Chnaging Items in a List 
+#breakfastFoods[0] = "Sausage"
+#testScores[0] = 97
+#classGPA[0] = 3.57
+# print(breakfastFoods[0])
+# print(testScores[0])
+# print(classGPA[0])
+# print(breakfastFoods)
+# print(testScores)
+# print(classGPA)
+
+# Pause -- WYOC -- Change 5th Element
+breakfastFoods[0] = "Sausage"
+testScores[0] = 97
+classGPA[0] = 3.57
+
+
+
