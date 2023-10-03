6b7ad57ed231ea791897f5e52df8818077f2ca73
s11703667 <142252946+s11703667@users.noreply.github.com>
2023-10-03 11:30:21 -0400

    Update collections.py

---
+++ gaming_exercises/00-numberGuess/00-collections/collections.py
@@ -0,0 +1,9 @@
+# Collections Examples, London Baldwin, v0.0
+
+# LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES
+breakfastFoods = ["Bacon", "Waffles" "Pancakes", "Cereal" , "Milk"]
+# Each item on the list is known as an ELEMENT.
+# The position in the list for each is a INDEX.
+# The elemnet "Bacon" is at index 0.
+# Python Only: index -1 it is the last item on the list.
+testScores = []
