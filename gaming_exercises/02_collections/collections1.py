3044668a9d430535fada2bb5b8ed9d876bd43e26
s11703667 <142252946+s11703667@users.noreply.github.com>
2023-10-05 11:40:06 -0400

    Update collections1.py

--- gaming_exercises/02_collections/collections1.py
+++ gaming_exercises/02_collections/collections1.py
@@ -1,15 +1,29 @@
 # Adding Items
-playerInventory = []
-while len(playerInventory) < 10:
-    item = input("What do you want to add?\n")
-    playerInventory.append(item) # .append() adds to the end of the list.
-playerInventory.sort()
-# .whatever() is known as a METHOD. IT MEANS "DO THIS TO THAT".
-print(playerInventory)
+# playerInventory = []
+# while len(playerInventory) < 10:
+#     item = input("What do you want to add?\n")
+#     playerInventory.append(item) # .append() adds to the end of the list.
+# playerInventory.sort()
+# # .whatever() is known as a METHOD. IT MEANS "DO THIS TO THAT".
+# print(playerInventory)
 
-# REMOVE ITEMS
-while len(playerInventory) > 5:
-    item = input("What do you want to remove?\n")
-    playerInventory.remove(item)
-playerInventory.sort()
-print(playerInventory)
+# # REMOVE ITEMS
+# while len(playerInventory) > 5:
+#     item = input("What do you want to remove?\n")
+#     playerInventory.remove(item)
+# playerInventory.sort()
+# print(playerInventory)
+
+# Lists For Fixed Inventory System
+weaponList = [
+    False, # Flame thrower
+    False, # Pistol
+    False, # Swiss blade
+    False, # Bazooka
+    False # Banana
+]
+# each item/value in a list is an ELEMENT
+# the location of each element in the list is the INDEX
+# first element is index[0]
+# shortcut to last element is index[-1]
+
