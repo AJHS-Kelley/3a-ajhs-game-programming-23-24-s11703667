29cc4abb552087b30809037c7c26102c8937ae61
s11703667 <142252946+s11703667@users.noreply.github.com>
2023-10-05 12:59:28 -0400

    Update collections1.py

--- gaming_exercises/02_collections/collections1.py
+++ gaming_exercises/02_collections/collections1.py
@@ -15,28 +15,40 @@
 # print(playerInventory)
 
 # Lists For Fixed Inventory System
-weaponList = [
-    False, # Flame thrower
-    False, # Pistol
-    True, # Swiss blade
-    False, # Bazooka
-    True # Banana
+# weaponList = [
+#     False, # Flame thrower
+#     False, # Pistol
+#     True, # Swiss blade
+#     False, # Bazooka
+#     True # Banana
+# ]
+# # each item/value in a list is an ELEMENT
+# # the location of each element in the list is the INDEX
+# # first element is index[0]
+# # shortcut to last element is index[-1]
+# weaponNum = 0
+# while weaponNum < len(weaponList):
+#     if weaponNum == 0 and weaponList[0] == True:
+#         print("Your Character is equipped with a Flame thrower.\n")
+#     if weaponNum == 0 and weaponList[1] == True:
+#         print("Your Character is equipped with a Pistol.\n")
+#     if  weaponNum == 0 and weaponList[2] == True:
+#         print("Your Character is equipped with a Swiss Blade.\n")
+#     if weaponNum == 0 and weaponList[3] == True:
+#         print("Your Character is equipped with a Bazooka.\n")
+#     if weaponNum == 0 and weaponList[4] == True:
+#         print("Your Character is equipped with a Banana.\n")
+#     weaponNum += 1
+
+# ITEM EXISTS IN INENTORY
+doorKeys = [
+    "red",
+    "blue",
+    "green",
+    "yellow"
 ]
-# each item/value in a list is an ELEMENT
-# the location of each element in the list is the INDEX
-# first element is index[0]
-# shortcut to last element is index[-1]
-weaponNum = 0
-while weaponNum < len(weaponList):
-    if weaponNum == 0 and weaponList[0] == True:
-        print("Your Character is equipped with a Flame thrower.\n")
-    if weaponNum == 0 and weaponList[1] == True:
-        print("Your Character is equipped with a Pistol.\n")
-    if  weaponNum == 0 and weaponList[2] == True:
-        print("Your Character is equipped with a Swiss Blade.\n")
-    if weaponNum == 0 and weaponList[3] == True:
-        print("Your Character is equipped with a Bazooka.\n")
-    if weaponNum == 0 and weaponList[4] == True:
-        print("Your Character is equipped with a Banana.\n")
-    weaponNum += 1
-    
+item = input("Which key do you require?").lower()
+if item in doorKeys:
+    print(f"You have the {item} key!\n")
+else:
+    print(f"You do not have the {item} key.")
