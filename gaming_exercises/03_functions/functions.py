13f3fca39c7fc54d9774b2863b8ddb65ef7dd2e7
s11703667 <142252946+s11703667@users.noreply.github.com>
2023-10-20 11:33:36 -0400

    Create functions.py

---
+++ gaming_exercises/03_functions/functions.py
@@ -0,0 +1,9 @@
+
+
+def functionName(): # Fuction Signature
+    print("Whats your name?\n")
+    name = input("Please type it and press enter.\n")
+    print(f"Hello, {name}\n")
+
+# Call the Function
+functionName()
