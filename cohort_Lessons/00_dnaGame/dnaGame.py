7ddf3ff4bc66e6da814c124221ed383edb984759
s11703667 <142252946+s11703667@users.noreply.github.com>
2024-01-10 12:06:08 -0500

    Create dnaGame.py

---
+++ cohort_Lessons/00_dnaGame/dnaGame.py
@@ -0,0 +1,12 @@
+# DNA Replication Game, London baldwin, v0.0a
+
+# Import Entire Module
+import time, datetime # BRING THE WHOLE TOOL BOX
+
+# Import Specific Method from a Module
+from random import choice # BRING JUST THE TOOL YOU NEED
+
+dnaBases = ["A", "T", "G", "C"] # Adenine, Thymine, Guanine, Cytosine
+
+
+
