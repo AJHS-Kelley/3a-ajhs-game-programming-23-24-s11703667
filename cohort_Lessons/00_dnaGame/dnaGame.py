a6e33c571f2e39af197f09459ee45205a739a994
s11703667 <142252946+s11703667@users.noreply.github.com>
2024-01-10 13:03:16 -0500

    Dna Loop

--- cohort_Lessons/00_dnaGame/dnaGame.py
+++ cohort_Lessons/00_dnaGame/dnaGame.py
@@ -8,5 +8,18 @@ from random import choice # BRING JUST THE TOOL YOU NEED
 
 dnaBases = ["A", "T", "G", "C"] # Adenine, Thymine, Guanine, Cytosine
 
+def gameIntro() -> None:
+    pass
 
+def genDNA() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("Please enter a whole number for the number of bases to generate.\n"))
+    dnaSequence = ""
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+    return dnaSequence
+
+# dna = genDNA()
+# print(dna)
 
